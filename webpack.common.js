d76e68578880277874e8da76e4802d5d248772b0
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-09-22 22:56:14 +0800

    ⚒️build: install and setup webpack

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'src/js/index.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Dashboard',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/views/index.html'),
+    }),
+
+    new CleanWebpackPlugin(),
+  ],
+};
