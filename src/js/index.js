94b6c546c44a3569d2189be063c27d5e1609498e
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-09-23 12:45:23 +0800

    ⚒️build: set main min height

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,17 @@ import * as bootstrap from 'bootstrap';
 
 // Import components
 import './components';
+
+const initPages = () => {
+  const header = document.querySelector('header');
+  const main = document.querySelector('main');
+  const footer = document.querySelector('footer');
+
+  if (header && main && footer) {
+    main.style.minHeight = `calc(100vh - ${header.clientHeight + footer.clientHeight}px)`;
+  }
+};
+
+window.addEventListener('DOMContentLoaded', async () => {
+  initPages();
+});
