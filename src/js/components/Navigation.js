b0f005e99ff4c50d53d731c9e1cb1f6156eba3eb
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-09-23 12:10:49 +0800

    ⚒️build: make and implement Offcanvas component

--- src/js/components/Navigation.js
+++ src/js/components/Navigation.js
@@ -22,6 +22,9 @@ class Navigation extends LitWithoutShadowDom {
       <nav class="navbar">
         <section class="container">
           <a href="/" class="navbar-brand fw-bold text-uppercase fs-4">${this.brandName}</a>
+          <section class="d-flex">
+            <off-canvas class="ms-auto mb-2 mb-md-0"></off-canvas>
+          </section>
         </section>
       </nav>
     `;
---
+++ src/js/components/Offcanvas.js
@@ -0,0 +1,52 @@
+import { html } from 'lit';
+import LitWithoutShadowDom from './base/LitWithoutShadowDom';
+
+class Offcanvas extends LitWithoutShadowDom {
+  static properties = {
+    brandName: { type: String, reflect: true },
+  };
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <button
+        class="btn p-0"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasWithBothOptions"
+        aria-controls="offcanvasWithBothOptions"
+      >
+        <i class="bi bi-list fs-3"></i>
+      </button>
+
+      <section
+        class="offcanvas offcanvas-end"
+        data-bs-scroll="true"
+        tabindex="-1"
+        id="offcanvasWithBothOptions"
+        aria-labelledby="offcanvasWithBothOptionsLabel"
+      >
+        <section class="offcanvas-header">
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </section>
+        <section class="offcanvas-body"></section>
+      </section>
+    `;
+  }
+
+  _userLogOut(event) {
+    event.preventDefault();
+    Utils.destroyUserToken(Config.USER_TOKEN_KEY);
+    CheckUserAuth.checkLoginState();
+  }
+}
+
+customElements.define('off-canvas', Offcanvas);
